2d6a86ef23cf1206bc2986644f64812e1a0ca06d
M.Islam <113365230+theMIslam@users.noreply.github.com>
2023-04-03 17:37:59 +0600

    six commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,14 +2,28 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.repository.db.ArticleDataBase
+import com.example.newsapp.viewModel.NewsViewModel
+import com.example.newsapp.viewModel.NewsViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDataBase(this))
+        val viewModelProvider = NewsViewModelFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProvider).get(NewsViewModel::class.java)
+
+        bottomNavigationView.setupWithNavController(newsFragment.findNavController())
     }
 }
--- app/src/main/java/com/example/newsapp/model/Article.kt
+++ app/src/main/java/com/example/newsapp/model/Article.kt
@@ -1,8 +1,13 @@
 package com.example.newsapp.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+
+@Entity(
+    tableName = "articles"
+)
 
 data class Article(
     @PrimaryKey(autoGenerate = true)
