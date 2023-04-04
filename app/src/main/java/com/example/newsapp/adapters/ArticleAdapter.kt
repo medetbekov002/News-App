fdd72ba48ffb58ed04bd62fc34a155b50c1f1580
M.Islam <113365230+theMIslam@users.noreply.github.com>
2023-04-04 12:25:15 +0600

    eight commit (error adapters and fragments + viewmodel

--- app/src/main/java/com/example/newsapp/adapters/ArticleAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/ArticleAdapter.kt
@@ -1,4 +1,120 @@
 package com.example.newsapp.adapters
 
-class ArticleAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.util.Util
+import com.example.newsapp.R
+import com.example.newsapp.databinding.ItemArticleBinding
+import com.example.newsapp.model.Article
+
+
+class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+
+    companion object {
+        private val diffUtilCallback = object : DiffUtil.ItemCallback<Article>() {
+            override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem.title == oldItem.title
+            }
+
+            override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return newItem.title == newItem.title
+            }
+
+        }
+    }
+
+    class ArticleViewHolder(var view: ItemArticleBinding) : RecyclerView.ViewHolder(view.root)
+
+    val differ = AsyncListDiffer(this, diffUtilCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<ItemArticleBinding>(
+            inflater,
+            R.layout.item_article,
+            parent,
+            false
+        )
+        return ArticleViewHolder(view)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.view.article = article
+
+        // item click listener
+        // bind these click listener later
+        holder.itemView.setOnClickListener {
+            onItemClickListener.let {
+                article.let { article ->
+                    it(article)
+                }
+            }
+        }
+
+        holder.view.ivShare.setOnClickListener {
+            onShareNewsClick.let {
+                article.let { article ->
+                    it(article)
+                }
+            }
+        }
+
+        holder.view.ivSave.setOnClickListener {
+            if (holder.view.ivSave.tag.toString().toInt() == 0) {
+                holder.view.ivSave.tag = 1
+                holder.view.ivSave.setImageDrawable(it.resources.getDrawable(R.drawable.ic_saved))
+                onArticleSaveClick?.let {
+                    if (article != null) {
+                        it(article)
+                    }
+                }
+            } else {
+                holder.view.ivSave.tag = 0
+                holder.view.ivSave.setImageDrawable(it.resources.getDrawable(R.drawable.ic_save))
+                onArticleSaveClick?.let {
+                    if (article != null) {
+                        it(article)
+                    }
+                }
+            }
+            onArticleSaveClick?.let {
+                article?.let{itl -> it(itl)}
+            }
+        }
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+    var isSave = false
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    private var onItemClickListener: ((Article)) -> Util)? = null
+    private var onArticleSaveClick: ((Article)) -> Util)? = null
+    private var onArticleDeleteClick: ((Article)) -> Util)? = null
+    internal var onShareNewsClick: ((Article)) -> Util)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    fun onSaveClickListener(listener: (Article) -> Unit) {
+        onArticleSaveClick = listener
+    }
+
+    fun onDeleteClickListener(listener: (Article) -> Unit) {
+        onArticleDeleteClick = listener
+    }
+
+    fun onShareClickListener(listener: ((Article) -> Util)) {
+        onShareNewsClick = listener
+    }
+
 }
--- app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
@@ -1,20 +1,35 @@
 package com.example.newsapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.newsapp.MainActivity
 import com.example.newsapp.R
+import com.example.newsapp.viewModel.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+    lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by lazy navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewmodel = (activity as MainActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageCommitVisible(view: WebView?, url: String?) {
+                    super.onPageCommitVisible(view, url)
+                    progressBar.visibility = View.GONE
+                }
+            }
+            loadUrl(article.url.toString())
+
+        }
     }
-
 }
+
